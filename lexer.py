bb2a81ed41ca9887b6c54447e3f72f8ba9f2233e
Jason T. Bradshaw <jasontbradshaw@gmail.com>
2011-02-04 00:44:57 -0600

    generalized open and close symbols, added better repr printing to classes, created classes for floats and ints

--- lexer.py
+++ lexer.py
@@ -2,6 +2,10 @@
 
 import re
 
+OPEN_PAREN = "("
+CLOSE_PAREN = ")"
+QUOTE = "'"
+
 class Symbol:
     def __init__(self, symbol_string):
         self.value = symbol_string
@@ -10,9 +14,29 @@ class Symbol:
         return self.value
 
     def __repr__(self):
-        return str(self)
+        return "(SYMBOL " + str(self) + ")"
 
-class LexingError(Exception):
+class Integer:
+    def __init__(self, integer_string):
+        self.value = int(integer_string)
+
+    def __str__(self):
+        return str(self.value)
+
+    def __repr__(self):
+        return "(INTEGER " + str(self) + ")"
+
+class Float:
+    def __init__(self, float_string):
+        self.value = float(float_string)
+
+    def __str__(self):
+        return str(self.value)
+
+    def __repr__(self):
+        return "(FLOAT " + str(self) + ")"
+
+class ParserError(Exception):
     """ Raised when parsing input fails. """
 
 def parse(s):
@@ -25,7 +49,8 @@ def tokenize(s):
     """
 
     # this is simple and fast. thanks Norvig!
-    return s.replace("(", " ( ").replace(")", " ) ").split()
+    return s.replace(OPEN_PAREN, " " + OPEN_PAREN + " ").replace(
+            CLOSE_PAREN, " " + CLOSE_PAREN + " ").split()
 
 def lex(tokens):
     """
@@ -36,20 +61,21 @@ def lex(tokens):
         raise ValueError("Invalid input.")
 
     token = tokens.pop(0)
-    if token == "(":
+    if token == OPEN_PAREN:
         e = []
-        while len(tokens) > 0 and tokens[0] != ")":
+        while len(tokens) > 0 and tokens[0] != CLOSE_PAREN:
             e.append(lex(tokens))
 
-        # pop off ')'
+        # pop off ')' if it exists
         if len(tokens) > 0:
             tokens.pop(0)
         else:
-            raise LexingError("Expecting closing parenthesis in expression.")
+            raise ParserError("Expecting closing parenthesis in expression.")
 
+        # return the lexed input stream
         return e
-    elif token == ")":
-        raise LexingError("Found unexpected closing parenthesis in expression.")
+    elif token == CLOSE_PAREN:
+        raise ParserError("Found unexpected closing parenthesis in expression.")
     else:
         return atomize(token)
 
@@ -60,17 +86,17 @@ def atomize(symbol):
 
     # attempt to return an int
     try:
-        return int(symbol)
+        return Integer(symbol)
     except ValueError:
         pass
 
     # attempt to return a float
     try:
-        return float(symbol)
+        return Float(symbol)
     except ValueError:
         pass
 
-    # otherwise, return a symbol
+    # otherwise, just return a symbol
     return Symbol(symbol)
 
 if __name__ == "__main__":

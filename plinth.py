0b6a4a718f97d60d56ad9a1fb3b8254fdac80629
Jason T. Bradshaw <jasontbradshaw@gmail.com>
2012-09-05 20:35:57 -0500

    Extract primitive functions to a module

--- plinth.py
+++ plinth.py
@@ -1,299 +1,68 @@
 #!/usr/bin/env python
 
-import math
-import numbers
 import os
 
 import errors
 import lang
+import primitives
 import tokens
 import util
-
-# used to get generate guaranteed-unique symbol names
-GENSYM_COUNTER = util.ThreadSafeCounter()
-
-def add(a, b, *rest):
-    """Adds the all the given numbers together."""
-
-    util.ensure_type(numbers.Number, a, b)
-
-    # add all the arguments together while checking type
-    total = a + b
-    for n in rest:
-        util.ensure_type(numbers.Number, n)
-        total += n
-
-    return total
-
-def sub(a, b, *rest):
-    """Subtracts the given numbers in sequence."""
-
-    util.ensure_type(numbers.Number, a, b)
-
-    # subtract all the arguments in sequence while checking type
-    difference = a - b
-    for n in rest:
-        util.ensure_type(numbers.Number, n)
-        difference -= n
-
-    return difference
-
-def mul(a, b, *rest):
-    """Multiplies all the given numbers together."""
-
-    util.ensure_type(numbers.Number, a, b)
-
-    # multiply all the arguments together while checking type
-    product = a * b
-    for n in rest:
-        # stop multiplying if the product ever goes to zero
-        if product == 0:
-            break
-
-        util.ensure_type(numbers.Number, n)
-        product *= n
-
-    return product
-
-def div(a, b, *rest):
-    """Divides the given numbers in sequence."""
-
-    util.ensure_type(numbers.Number, a, b)
-
-    # divide all the arguments in sequence while checking type
-    quotient = a / b
-    for n in rest:
-        util.ensure_type(numbers.Number, n)
-        quotient /= n
-
-    return quotient
-
-def power(a, b):
-    """Raises a to the power of b."""
-    util.ensure_type(numbers.Number, a, b)
-    return a ** b
-
-def sin(a):
-    """Takes the sine of a."""
-    util.ensure_type(numbers.Number, a)
-    return math.sin(a)
-
-def cos(a):
-    """Takes the cosine of a."""
-    util.ensure_type(numbers.Number, a)
-    return math.cos(a)
-
-def tan(a):
-    """Takes the tangent of a."""
-    util.ensure_type(numbers.Number, a)
-    return math.tan(a)
-
-def atan(a):
-    """Takes the arctangent of a."""
-    util.ensure_type(numbers.Number, a)
-    return math.atan(a)
-
-def atan2(a):
-    """Takes the second arctangent of a."""
-    util.ensure_type(numbers.Number, a)
-    return math.atan2(a)
-
-def type_(e):
-    """Returns the type of an element as a string. Returns 'nil' for NIL."""
-
-    if isinstance(e, lang.Symbol):
-        return lang.String("symbol")
-    elif isinstance(e, lang.String):
-        return lang.String("string")
-    elif isinstance(e, lang.Boolean):
-        return lang.String("boolean")
-    elif isinstance(e, (int, long)):
-        return lang.String("integer")
-    elif isinstance(e, float):
-        return lang.String("float")
-    elif isinstance(e, complex):
-        return lang.String("complex")
-    elif e is lang.NIL:
-        return lang.String("nil")
-    elif isinstance(e, lang.Cons):
-        return lang.String("cons")
-    elif isinstance(e, lang.Function):
-        return lang.String("function")
-    elif isinstance(e, lang.Macro):
-        return lang.String("macro")
-
-    # shouldn't ever get this far
-    raise errors.WrongArgumentTypeError("unsupported type: " +
-            e.__class__.__name__.lower())
-
-def is_(a, b):
-    """Returns true if the two items refer to the same object in memory."""
-    return lang.Boolean.build(a is b)
-
-def equal(a, b):
-    """
-    Returns true if two constructs are congruent. For example, numbers are
-    compared mathematically, cons are compared by structure and equivalent
-    contents, etc.
-    """
-
-    # the same item is equal to itself
-    if a is b:
-        return lang.TRUE
-
-    # things can't be equal if they're not the same class
-    elif not (isinstance(a, b.__class__) and isinstance(b, a.__class__)):
-        return lang.FALSE
-
-    # compare everything else by value (numbers, Cons, symbols, etc.)
-    return lang.Boolean.build(a == b)
-
-def gt(a, b):
-    """Compare two numbers using '>'."""
-    util.ensure_type(numbers.Number, a, b)
-    return lang.Boolean.build(a > b)
-
-def gte(a, b):
-    """Compare two numbers using '>='."""
-    util.ensure_type(numbers.Number, a, b)
-    return lang.Boolean.build(a >= b)
-
-def lt(a, b):
-    """Compare two numbers using '<'."""
-    util.ensure_type(numbers.Number, a, b)
-    return lang.Boolean.build(a < b)
-
-def lte(a, b):
-    """Compare two numbers using '<='."""
-    util.ensure_type(numbers.Number, a, b)
-    return lang.Boolean.build(a <= b)
-
-def not_(a):
-    """
-    Returns the opposite boolean of that passed in. All things that aren't #f
-    are #t, so we return whether a is False.
-    """
-
-    return lang.Boolean.build(a is lang.FALSE)
-
-def cons(a, b):
-    """Pair two items."""
-    return lang.Cons(a, b)
-
-def car(e):
-    """Return the first element of a pair."""
-    util.ensure_type(lang.Cons, e)
-
-    # nil isn't allowed to be indexed into, since it has no car or cdr
-    if e is lang.NIL:
-        raise errors.WrongArgumentTypeError("wrong argument type for car: " +
-                "expected pair, got " + str(e))
-
-    return e.car
-
-def cdr(e):
-    """Return the second element of a pair."""
-    util.ensure_type(lang.Cons, e)
-
-    if e is lang.NIL:
-        raise errors.WrongArgumentTypeError("wrong argument type for cdr: " +
-                "expected pair, got " + str(e))
-
-    return e.cdr
-
-def read(prompt):
-    """Print the prompt, read input from stdin, and return it as a string."""
-    util.ensure_type(basestring, prompt)
-    return String(raw_input(prompt))
-
-def parse_(s):
-    """Parse a string into a list of the S-expressions it describes."""
-    util.ensure_type(basestring, s)
-    return lang.Cons.build(*parse(tokens.tokenize(s)))
-
-def gensym(prefix):
-    """
-    Generate a unique symbol with the given prefix in its name. Generated
-    symbols have names that contain syntax elements, and hence can't be entered
-    via the reader.
-    """
-    util.ensure_type(basestring, prefix)
-    return lang.Symbol(prefix + tokens.OPEN_PAREN + str(GENSYM_COUNTER()) +
-            tokens.CLOSE_PAREN)
-
-# these functions serve as markers for whether the function being called is
-# special. we check to see if the function for the symbol is one of these
-# functions, and if so we evaluate it in whatever way it requires. this allows
-# the user to define new symbols that point to these functions, but still have
-# the functions work in the same way.
-quote = lang.PrimitiveFunction(lambda e: _, name=tokens.QUOTE_LONG)
-unquote = lang.PrimitiveFunction(lambda e: _, name=tokens.UNQUOTE_LONG)
-quasiquote = lang.PrimitiveFunction(lambda e: _, name=tokens.QUASIQUOTE_LONG)
-lambda_ = lang.PrimitiveFunction(lambda args, body: _, name=tokens.LAMBDA)
-macro = lang.PrimitiveFunction(lambda args, body: _, name=tokens.MACRO)
-expand = lang.PrimitiveFunction(lambda macro, *args: _, name=tokens.MACRO_EXPAND)
-define = lang.PrimitiveFunction(lambda symbol, value: _, name=tokens.DEFINE)
-cond = lang.PrimitiveFunction(lambda *e: _, name=tokens.COND)
-and_ = lang.PrimitiveFunction(lambda a, b, *rest: _, name=tokens.AND)
-or_ = lang.PrimitiveFunction(lambda a, b, *rest: _, name=tokens.OR)
-eval_ = lang.PrimitiveFunction(lambda sexp: _, name=tokens.EVAL)
-load = lang.PrimitiveFunction(lambda fname: _, name=tokens.LOAD)
 
 # the base environment for the interpreter
 GLOBAL_ENV = lang.Environment(None)
 
 # functions that need special treatment during evaluation
-GLOBAL_ENV[lang.Symbol(tokens.QUOTE_LONG)] = quote
-GLOBAL_ENV[lang.Symbol(tokens.QUASIQUOTE_LONG)] = quasiquote
-GLOBAL_ENV[lang.Symbol(tokens.LAMBDA)] = lambda_
-GLOBAL_ENV[lang.Symbol(tokens.MACRO)] = macro
-GLOBAL_ENV[lang.Symbol(tokens.MACRO_EXPAND)] = expand
-GLOBAL_ENV[lang.Symbol(tokens.DEFINE)] = define
-GLOBAL_ENV[lang.Symbol(tokens.COND)] = cond
-GLOBAL_ENV[lang.Symbol(tokens.AND)] = and_
-GLOBAL_ENV[lang.Symbol(tokens.OR)] = or_
-GLOBAL_ENV[lang.Symbol(tokens.EVAL)] = eval_
-GLOBAL_ENV[lang.Symbol(tokens.LOAD)] = load
+GLOBAL_ENV[lang.Symbol(tokens.QUOTE_LONG)] = primitives.quote
+GLOBAL_ENV[lang.Symbol(tokens.QUASIQUOTE_LONG)] = primitives.quasiquote
+GLOBAL_ENV[lang.Symbol(tokens.LAMBDA)] = primitives.lambda_
+GLOBAL_ENV[lang.Symbol(tokens.MACRO)] = primitives.macro
+GLOBAL_ENV[lang.Symbol(tokens.MACRO_EXPAND)] = primitives.expand
+GLOBAL_ENV[lang.Symbol(tokens.DEFINE)] = primitives.define
+GLOBAL_ENV[lang.Symbol(tokens.COND)] = primitives.cond
+GLOBAL_ENV[lang.Symbol(tokens.AND)] = primitives.and_
+GLOBAL_ENV[lang.Symbol(tokens.OR)] = primitives.or_
+GLOBAL_ENV[lang.Symbol(tokens.EVAL)] = primitives.eval_
+GLOBAL_ENV[lang.Symbol(tokens.LOAD)] = primitives.load
 
 # adds a new primitive function to the global environment
 ap = lambda t, f: GLOBAL_ENV.put(lang.Symbol(t), lang.PrimitiveFunction(f, t))
 
 # repl
-ap(tokens.READ, read)
-ap(tokens.PARSE, parse_)
+ap(tokens.READ, primitives.read)
+ap(tokens.PARSE, primitives.parse_)
 
 # logical
-ap(tokens.NOT, not_)
+ap(tokens.NOT, primitives.not_)
 
 # math
-ap(tokens.ADD, add)
-ap(tokens.SUBTRACT, sub)
-ap(tokens.MULTIPLY, mul)
-ap(tokens.DIVIDE, div)
-ap(tokens.POWER, power)
-ap(tokens.SIN, sin)
-ap(tokens.COS, cos)
-ap(tokens.TAN, tan)
-ap(tokens.ARCTAN, atan)
-ap(tokens.ARCTAN2, atan2)
+ap(tokens.ADD, primitives.add)
+ap(tokens.SUBTRACT, primitives.sub)
+ap(tokens.MULTIPLY, primitives.mul)
+ap(tokens.DIVIDE, primitives.div)
+ap(tokens.POWER, primitives.power)
+ap(tokens.SIN, primitives.sin)
+ap(tokens.COS, primitives.cos)
+ap(tokens.TAN, primitives.tan)
+ap(tokens.ARCTAN, primitives.atan)
+ap(tokens.ARCTAN2, primitives.atan2)
 
 # comparison
-ap(tokens.IS, is_)
-ap(tokens.EQUAL, equal)
-ap(tokens.GREATER_THAN, gt)
-ap(tokens.GREATER_THAN_EQUAL, gte)
-ap(tokens.LESS_THAN, lt)
-ap(tokens.LESS_THAN_EQUAL, lte)
+ap(tokens.IS, primitives.is_)
+ap(tokens.EQUAL, primitives.equal)
+ap(tokens.GREATER_THAN, primitives.gt)
+ap(tokens.GREATER_THAN_EQUAL, primitives.gte)
+ap(tokens.LESS_THAN, primitives.lt)
+ap(tokens.LESS_THAN_EQUAL, primitives.lte)
 
 # cons
-ap(tokens.CONS, cons)
-ap(tokens.CAR, car)
-ap(tokens.CDR, cdr)
+ap(tokens.CONS, primitives.cons)
+ap(tokens.CAR, primitives.car)
+ap(tokens.CDR, primitives.cdr)
 ap(tokens.LISTP, lang.Cons.is_list)
 
 # meta
-ap(tokens.GENERATE_SYMBOL, gensym)
-ap(tokens.TYPE, type_)
+ap(tokens.GENERATE_SYMBOL, primitives.gensym)
+ap(tokens.TYPE, primitives.type_)
 
 def parse(token_source):
     """
@@ -520,18 +289,18 @@ def evaluate(sexp, env):
                     str(function))
 
         # quote
-        if function is quote:
+        if function is primitives.quote:
             # return the argument unevaluated
             util.ensure_args(args, num_required=1)
             return args.car
 
         # quasiquote
-        elif function is quasiquote:
+        elif function is primitives.quasiquote:
             util.ensure_args(args, num_required=1)
             return quasiquote_evaluate(args.car, env)
 
         # function
-        elif function is lambda_:
+        elif function is primitives.lambda_:
             util.ensure_args(args, num_required=2)
 
             arg_symbols = args.car
@@ -541,7 +310,7 @@ def evaluate(sexp, env):
             return lang.Function(evaluate, env, arg_symbols, body)
 
         # macro
-        elif function is macro:
+        elif function is primitives.macro:
             util.ensure_args(args, num_required=2)
 
             arg_symbols = args.car
@@ -551,7 +320,7 @@ def evaluate(sexp, env):
             return lang.Macro(evaluate, env, arg_symbols, body)
 
         # macro expand
-        elif function is expand:
+        elif function is primitives.expand:
             util.ensure_args(args, num_required=1, is_variadic=True)
 
             # evaluate to get the macro and its arguments
@@ -564,7 +333,7 @@ def evaluate(sexp, env):
             return m(evaluate, env, *arg_expressions)
 
         # define
-        elif function is define:
+        elif function is primitives.define:
             util.ensure_args(args, num_required=2)
 
             symbol = args.car
@@ -587,7 +356,7 @@ def evaluate(sexp, env):
             return result
 
         # cond
-        elif function is cond:
+        elif function is primitives.cond:
             for tup in args:
                 # if e is not a list, len() raises an error for us
                 if len(tup) != 2:
@@ -608,7 +377,7 @@ def evaluate(sexp, env):
                     "evaluate to " + lang.TRUE)
 
         # logical and
-        elif function is and_:
+        elif function is primitives.and_:
             util.ensure_args(args, num_required=2, is_variadic=True)
 
             # evaluate the arguments, returning the final one if none were #f,
@@ -622,7 +391,7 @@ def evaluate(sexp, env):
             return last_item
 
         # logical or
-        elif function is or_:
+        elif function is primitives.or_:
             util.ensure_args(args, num_required=2, is_variadic=True)
 
             # evaluate the arguments, returning the first one that's not #f,
@@ -635,14 +404,14 @@ def evaluate(sexp, env):
             return last_item
 
         # eval
-        elif function is eval_:
+        elif function is primitives.eval_:
             util.ensure_args(args, num_required=1)
 
             # evaluate the given s-expression and return it
             return evaluate(evaluate(args.car, env), env)
 
         # load
-        elif function is load:
+        elif function is primitives.load:
             util.ensure_args(args, num_required=1)
             util.ensure_type(lang.String, args.car)
 
---
+++ primitives.py
@@ -0,0 +1,237 @@
+import math
+import numbers
+
+import errors
+import lang
+import tokens
+import util
+
+# used to get generate guaranteed-unique symbol names
+GENSYM_COUNTER = util.ThreadSafeCounter()
+
+def add(a, b, *rest):
+    """Adds the all the given numbers together."""
+
+    util.ensure_type(numbers.Number, a, b)
+
+    # add all the arguments together while checking type
+    total = a + b
+    for n in rest:
+        util.ensure_type(numbers.Number, n)
+        total += n
+
+    return total
+
+def sub(a, b, *rest):
+    """Subtracts the given numbers in sequence."""
+
+    util.ensure_type(numbers.Number, a, b)
+
+    # subtract all the arguments in sequence while checking type
+    difference = a - b
+    for n in rest:
+        util.ensure_type(numbers.Number, n)
+        difference -= n
+
+    return difference
+
+def mul(a, b, *rest):
+    """Multiplies all the given numbers together."""
+
+    util.ensure_type(numbers.Number, a, b)
+
+    # multiply all the arguments together while checking type
+    product = a * b
+    for n in rest:
+        # stop multiplying if the product ever goes to zero
+        if product == 0:
+            break
+
+        util.ensure_type(numbers.Number, n)
+        product *= n
+
+    return product
+
+def div(a, b, *rest):
+    """Divides the given numbers in sequence."""
+
+    util.ensure_type(numbers.Number, a, b)
+
+    # divide all the arguments in sequence while checking type
+    quotient = a / b
+    for n in rest:
+        util.ensure_type(numbers.Number, n)
+        quotient /= n
+
+    return quotient
+
+def power(a, b):
+    """Raises a to the power of b."""
+    util.ensure_type(numbers.Number, a, b)
+    return a ** b
+
+def sin(a):
+    """Takes the sine of a."""
+    util.ensure_type(numbers.Number, a)
+    return math.sin(a)
+
+def cos(a):
+    """Takes the cosine of a."""
+    util.ensure_type(numbers.Number, a)
+    return math.cos(a)
+
+def tan(a):
+    """Takes the tangent of a."""
+    util.ensure_type(numbers.Number, a)
+    return math.tan(a)
+
+def atan(a):
+    """Takes the arctangent of a."""
+    util.ensure_type(numbers.Number, a)
+    return math.atan(a)
+
+def atan2(a):
+    """Takes the second arctangent of a."""
+    util.ensure_type(numbers.Number, a)
+    return math.atan2(a)
+
+def type_(e):
+    """Returns the type of an element as a string. Returns 'nil' for NIL."""
+
+    if isinstance(e, lang.Symbol):
+        return lang.String("symbol")
+    elif isinstance(e, lang.String):
+        return lang.String("string")
+    elif isinstance(e, lang.Boolean):
+        return lang.String("boolean")
+    elif isinstance(e, (int, long)):
+        return lang.String("integer")
+    elif isinstance(e, float):
+        return lang.String("float")
+    elif isinstance(e, complex):
+        return lang.String("complex")
+    elif e is lang.NIL:
+        return lang.String("nil")
+    elif isinstance(e, lang.Cons):
+        return lang.String("cons")
+    elif isinstance(e, lang.Function):
+        return lang.String("function")
+    elif isinstance(e, lang.Macro):
+        return lang.String("macro")
+
+    # shouldn't ever get this far
+    raise errors.WrongArgumentTypeError("unsupported type: " +
+            e.__class__.__name__.lower())
+
+def is_(a, b):
+    """Returns true if the two items refer to the same object in memory."""
+    return lang.Boolean.build(a is b)
+
+def equal(a, b):
+    """
+    Returns true if two constructs are congruent. For example, numbers are
+    compared mathematically, cons are compared by structure and equivalent
+    contents, etc.
+    """
+
+    # the same item is equal to itself
+    if a is b:
+        return lang.TRUE
+
+    # things can't be equal if they're not the same class
+    elif not (isinstance(a, b.__class__) and isinstance(b, a.__class__)):
+        return lang.FALSE
+
+    # compare everything else by value (numbers, Cons, symbols, etc.)
+    return lang.Boolean.build(a == b)
+
+def gt(a, b):
+    """Compare two numbers using '>'."""
+    util.ensure_type(numbers.Number, a, b)
+    return lang.Boolean.build(a > b)
+
+def gte(a, b):
+    """Compare two numbers using '>='."""
+    util.ensure_type(numbers.Number, a, b)
+    return lang.Boolean.build(a >= b)
+
+def lt(a, b):
+    """Compare two numbers using '<'."""
+    util.ensure_type(numbers.Number, a, b)
+    return lang.Boolean.build(a < b)
+
+def lte(a, b):
+    """Compare two numbers using '<='."""
+    util.ensure_type(numbers.Number, a, b)
+    return lang.Boolean.build(a <= b)
+
+def not_(a):
+    """
+    Returns the opposite boolean of that passed in. All things that aren't #f
+    are #t, so we return whether a is False.
+    """
+
+    return lang.Boolean.build(a is lang.FALSE)
+
+def cons(a, b):
+    """Pair two items."""
+    return lang.Cons(a, b)
+
+def car(e):
+    """Return the first element of a pair."""
+    util.ensure_type(lang.Cons, e)
+
+    # nil isn't allowed to be indexed into, since it has no car or cdr
+    if e is lang.NIL:
+        raise errors.WrongArgumentTypeError("wrong argument type for car: " +
+                "expected pair, got " + str(e))
+
+    return e.car
+
+def cdr(e):
+    """Return the second element of a pair."""
+    util.ensure_type(lang.Cons, e)
+
+    if e is lang.NIL:
+        raise errors.WrongArgumentTypeError("wrong argument type for cdr: " +
+                "expected pair, got " + str(e))
+
+    return e.cdr
+
+def read(prompt):
+    """Print the prompt, read input from stdin, and return it as a string."""
+    util.ensure_type(basestring, prompt)
+    return String(raw_input(prompt))
+
+def parse_(s):
+    """Parse a string into a list of the S-expressions it describes."""
+    util.ensure_type(basestring, s)
+    return lang.Cons.build(*parse(tokens.tokenize(s)))
+
+def gensym(prefix):
+    """
+    Generate a unique symbol with the given prefix in its name. Generated
+    symbols have names that contain syntax elements, and hence can't be entered
+    via the reader.
+    """
+    util.ensure_type(basestring, prefix)
+    return lang.Symbol(prefix + tokens.OPEN_PAREN + str(GENSYM_COUNTER()) +
+            tokens.CLOSE_PAREN)
+
+# these functions serve as markers for whether the function being called is
+# special. we check to see if the function for the symbol is one of these
+# functions, and if so we evaluate it in whatever way it requires. this allows
+# the user to define new symbols that point to these functions, but still have
+# the functions work in the same way.
+quote = lang.PrimitiveFunction(lambda e: _, name=tokens.QUOTE_LONG)
+unquote = lang.PrimitiveFunction(lambda e: _, name=tokens.UNQUOTE_LONG)
+quasiquote = lang.PrimitiveFunction(lambda e: _, name=tokens.QUASIQUOTE_LONG)
+lambda_ = lang.PrimitiveFunction(lambda args, body: _, name=tokens.LAMBDA)
+macro = lang.PrimitiveFunction(lambda args, body: _, name=tokens.MACRO)
+expand = lang.PrimitiveFunction(lambda macro, *args: _, name=tokens.MACRO_EXPAND)
+define = lang.PrimitiveFunction(lambda symbol, value: _, name=tokens.DEFINE)
+cond = lang.PrimitiveFunction(lambda *e: _, name=tokens.COND)
+and_ = lang.PrimitiveFunction(lambda a, b, *rest: _, name=tokens.AND)
+or_ = lang.PrimitiveFunction(lambda a, b, *rest: _, name=tokens.OR)
+eval_ = lang.PrimitiveFunction(lambda sexp: _, name=tokens.EVAL)
+load = lang.PrimitiveFunction(lambda fname: _, name=tokens.LOAD)
